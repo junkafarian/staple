64f18420475f06f604b04f77dcf5dcc209f6cac3
junkafarian <fergus.doyle@largeblue.com>
2008-12-11 01:45:26 +0000

    class implementation

--- staple/publisher.py
+++ staple/publisher.py
@@ -2,49 +2,52 @@ from os import listdir, makedirs
 from os.path import abspath, isdir, isfile, join, exists
 from jinja2 import Environment, FileSystemLoader
 
-def publish(template_dir, output_dir='./', config_file=None):
+class Publisher:
     
-    # check values
-    if not isdir(template_dir):
-        raise Exception("Template path does not exist")
-    if not isdir(output_dir):
-        makedirs(output_dir)
+    def __init__(self, template_dir, output_dir='./', config_file=None):
+        # check values
+        if not isdir(template_dir):
+            raise Exception("Template path does not exist: %s" % template_dir)
+        if not isdir(output_dir):
+            makedirs(output_dir)
+        
+        self.template_dir = abspath(template_dir)
+        self.output_dir = abspath(output_dir)
+        
+        self.template_env = Environment(loader=FileSystemLoader(template_dir))
+        
+        if config_file:
+            # parse extra variables out of config file
+            pass
     
-    template_dir = abspath(template_dir)
-    output_dir = abspath(output_dir)
+    def __call__(self):
+        self.publishdir('./')
     
-    env = Environment(loader=FileSystemLoader(template_dir))
-    
-    if config_file:
-        # parse extra variables out of config file
-        pass
-    
-    def publishfile(path):
+    def publishfile(self, path):
         print 'publishing ', path
-        template = env.get_template(path)
+        template = self.template_env.get_template(path)
         
         # open the file
         try:
-            output = open(join(output_dir, path), 'w')
+            output = open(join(self.output_dir, path), 'w')
+            #print >>output, template.render()
             output.write(template.render())
             output.close()
         except IOError:
-            publishdir(path.rsplit('/', 1)[0])
-        
+            self.publishdir(path.rsplit('/', 1)[0])
     
-    def publishdir(path):
-        if not exists(join(output_dir, path)):
-            makedirs(join(output_dir, path))
-        current_dir = join(template_dir, path)
+    
+    def publishdir(self, path):
+        if not exists(join(self.output_dir, path)):
+            makedirs(join(self.output_dir, path))
+        current_dir = join(self.template_dir, path)
         for item in listdir(current_dir):
             if not(item.startswith('.') or item.startswith('_')):
                 new_path = '%s/%s' % (path, item)
-                new_abpath = join(template_dir, new_path)
+                new_abpath = join(self.template_dir, new_path)
                 if isdir(new_abpath):
-                    publishdir(new_path)
+                    self.publishdir(new_path)
                 elif isfile(new_abpath) and item.endswith('.html'):
-                    publishfile(new_path)
-    
-    publishdir('./')
+                    self.publishfile(new_path)
     
 
